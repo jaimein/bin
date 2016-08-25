96b9980e490d82fd16bf1f04b9044600df1265cd
jaimein <jaimeiserte@hotmail.com>
2016-08-25 11:55:08 +0200

    Añadido explicacion a comroot.sh

--- comproot.sh
+++ comproot.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+	### Para que solo root pueda ejecutar el script
+
 	if [ $LOGNAME != "root" ]
 	then
 	    read -p "Lo siento, este script debe ser ejecutado con privilegios de root."
